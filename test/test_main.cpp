3e47e5ba9481f0270ca268d194d7b1b427513211
u3653650 <u3653650+github@gmail.com>
2025-12-26 18:58:59 +0300

    Add files via upload

--- test/test_main.cpp
+++ test/test_main.cpp
@@ -7,7 +7,6 @@
 #include "WordCloud.h"
 #include "MaskApplier.h"
 
-// Тестирование загрузки шрифта
 TEST(LoadFontTest, ValidFontPath) {
     sf::Font font;
     EXPECT_TRUE(font.loadFromFile("opensans.ttf"));
@@ -18,7 +17,6 @@ TEST(LoadFontTest, InvalidFontPath) {
     EXPECT_FALSE(font.loadFromFile("invalid_font.ttf"));
 }
 
-// Тестирование создания RenderTexture
 TEST(CreateRenderTextureTest, ValidDimensions) {
     sf::RenderTexture renderTexture;
     EXPECT_TRUE(renderTexture.create(800, 600));
@@ -26,10 +24,9 @@ TEST(CreateRenderTextureTest, ValidDimensions) {
 
 TEST(CreateRenderTextureTest, InvalidDimensions) {
     sf::RenderTexture renderTexture;
-    EXPECT_FALSE(renderTexture.create(0, 0));  // Невозможные размеры
+    EXPECT_FALSE(renderTexture.create(0, 0));
 }
 
-// Тестирование генерации облака слов
 TEST(WordCloudTest, GenerateWordCloud) {
     sf::Font font;
     font.loadFromFile("opensans.ttf");
@@ -44,45 +41,33 @@ TEST(WordCloudTest, GenerateWordCloud) {
 
     wordCloud.generate(renderTexture);
 
-    // Пример: Проверим, что генерация не вызывает ошибок (например, не получаем пустую картинку)
     sf::Texture generatedTexture = renderTexture.getTexture();
     EXPECT_TRUE(generatedTexture.getSize().x > 0);
     EXPECT_TRUE(generatedTexture.getSize().y > 0);
 }
 
-// Тестирование функции applyMask
 TEST(ApplyMaskTest, ValidMask) {
     sf::Texture texture;
-    texture.loadFromFile("test_image.png");  // Замените на реальное изображение
-
-    // Проверим, что маска применяется корректно
+    texture.loadFromFile("test_image.png");
     EXPECT_NO_THROW(applyMask(texture, "tiger.png"));
 }
 
 TEST(ApplyMaskTest, InvalidMask) {
     sf::Texture texture;
-    texture.loadFromFile("test_image.png");  // Замените на реальное изображение
-
-    // Проверим, что при неправильном пути маски выбрасывается исключение или происходит ошибка
+    texture.loadFromFile("test_image.png");
     EXPECT_THROW(applyMask(texture, "invalid_mask.png"), std::runtime_error);
 }
 
-// Тестирование ImageSaver
 TEST(ImageSaverTest, SaveImageSuccess) {
     sf::RenderTexture renderTexture;
-    renderTexture.create(800, 600);  // Создаем render texture для теста
+    renderTexture.create(800, 600);
     sf::Texture texture = renderTexture.getTexture();
-
-    // Попробуем сохранить изображение
     EXPECT_TRUE(ImageSaver::saveImage(texture, "output.jpg"));
 }
 
 TEST(ImageSaverTest, SaveImageFailure) {
     sf::RenderTexture renderTexture;
-    renderTexture.create(800, 600);  // Создаем render texture для теста
+    renderTexture.create(800, 600);
     sf::Texture texture = renderTexture.getTexture();
-
-    // Пробуем сохранить в недоступный путь (например, в директорию без прав на запись)
     EXPECT_FALSE(ImageSaver::saveImage(texture, "/invalid/path/output.jpg"));
 }
-
